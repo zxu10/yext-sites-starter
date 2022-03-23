07c982313b81192933dae882955ced834d496343
Matt Kilpatrick <mkilpatrick@yext.com>
2022-03-23 17:45:24 -0600

    Add CTA component

---
+++ src/components/cta.tsx
@@ -0,0 +1,19 @@
+type Cta = {
+  buttonText: string;
+  url: string;
+}
+
+const Cta = (props: Cta) => {
+  const {
+    buttonText,
+    url,
+  } = props;
+
+  return (
+    <a href={url} className="py-4 px-6 bg-blue-400 text-base font-bold text-white" target="_blank" rel="noopener noreferrer">
+      {buttonText}
+    </a>
+  );
+};
+
+export default Cta;
