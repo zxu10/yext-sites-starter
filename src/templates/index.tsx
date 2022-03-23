00914540c46eed169e61211fd1a6b7e0c4918f3e
Matt Kilpatrick <mkilpatrick@yext.com>
2022-03-23 17:48:42 -0600

    Add CTA to index

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,4 +1,5 @@
 import Banner, { Address } from "../components/banner";
+import Cta from "../components/cta";
 
 // export const getServerSideProps = () => {
 //     return { message: "Hello from the server!" };
@@ -30,7 +31,7 @@ type Props = {
   name: string;
   address: Address;
 };
-  
+
 const Homepage = (props: any) => {
   const {
     name,
@@ -40,9 +41,14 @@ const Homepage = (props: any) => {
 
   return (
     <>
-      <Banner name={name} address={address} openTime={openTime} />
+      <Banner name={name} address={address} openTime={openTime}>
+        <div className="bg-white h-40 w-1/5 flex items-center justify-center text-center flex-col space-y-4">
+          <div className="text-black text-base">Visit Us Today!</div>
+          <Cta buttonText="Get Directions" url="http://google.com" />
+        </div>
+      </Banner>
     </>
   );
 };
-  
+
 export default Homepage;
